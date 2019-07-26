19567e2816fc63d66cc9742c4be37b7338163fa5
dmknyazev <53207539+dmknyazev@users.noreply.github.com>
2019-07-26 19:02:45 +0300

    Commit Agro

---
+++ agro/js/script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let slideIndex = 1,
+	slides = document.querySelectorAll('.slider__item'),
+	dotsWrap = document.querySelector('.slider__toggles'),
+	dot = document.querySelectorAll('.slider__toggle');
+
+showSlides(slideIndex);
+function showSlides(n) {
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+
+	slides.forEach((item) => item.style.display = 'none');
+
+	dot.forEach((item) => item.classList.remove('slider__toggle--active'));
+
+	slides[slideIndex - 1].style.display = 'block';
+	dot[slideIndex - 1].classList.add('slider__toggle--active');
+}
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+dotsWrap.addEventListener('click', function(event) {
+	for (let i = 0; i < dot.length + 1; i++) {
+		if (event.target.classList.contains('slider__toggle') && event.target == dot[i-1]) {
+			currentSlide(i);
+		}
+	}
+});
+
+
+
+let reviewsBlock = document.querySelector('.reviews');
+
+dot[0].addEventListener('click', function() {
+	reviewsBlock.style.backgroundImage = 'url(img/reviews-bg1.jpg)'
+});
+
+dot[1].addEventListener('click', function() {
+	reviewsBlock.style.backgroundImage = 'url(img/reviews-bg2.jpg)'
+});
+
+dot[2].addEventListener('click', function() {
+	reviewsBlock.style.backgroundImage = 'url(img/reviews-bg3.jpg)'
+});
