fea2ad9d90a519b9144806886e6dcb546ee3fdf6
dmknyazev <53207539+dmknyazev@users.noreply.github.com>
2019-11-28 00:06:22 +0300

    travel blog

--- travelblog/js/main.js
+++ travelblog/js/main.js
@@ -1,15 +1,34 @@
-'use strict';
 
-let searchWrapper = document.querySelector('.search'),
-	searchForm = document.querySelector('.search__form');
 
-searchWrapper.addEventListener('click', function() {
-	
-	if (searchForm.classList.contains('no-active')) {
-		searchForm.classList.remove('no-active')
-		searchForm.classList.add('active');
-	} else {
-		searchForm.classList.remove('active');
-		searchForm.classList.add('no-active');
-	}
+$(function(){
+
+	'use strict';
+
+	let searchWrapper = document.querySelector('.search'),
+		searchForm = document.querySelector('.search__form');
+
+	searchWrapper.addEventListener('click', function() {
+		
+		if (searchForm.classList.contains('no-active')) {
+			searchForm.classList.remove('no-active')
+			searchForm.classList.add('active');
+		} else {
+			searchForm.classList.remove('active');
+			searchForm.classList.add('no-active');
+		}
+	});
+
+  $('.slider__list').slick({
+  	arrows: false,
+    infinite: false,
+    dots: true
+  });
+
+  $('select').styler();
+
+  $('.header__btn-menu').on('click', function(){
+    $('.menu ul').slideToggle();
+  });
+
+
 });
